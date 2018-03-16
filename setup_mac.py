b04c1c40a6777332c70e3b0ad360d4d732a52f9c
Liz Cooper-Williams <e.cooperwilliams@uq.edu.au>
2018-03-16 17:21:38 +1000

    Mods to MAC setup

--- setup_mac.py
+++ setup_mac.py
@@ -1,37 +1,58 @@
-"""
+'''
+    QBI Auto Analysis APP: setup_mac.py (Mac OSX package)
+    *******************************************************************************
+    Copyright (C) 2017  QBI Software, The University of Queensland
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
 Usage:
     python3 setup.py py2app
 
     [ref: http://doc.qt.io/qt-4.8/deployment-mac.html]
-"""
+'''
 import sys
 from os.path import join
+from os import getcwd
+#Self -bootstrapping https://py2app.readthedocs.io
+import ez_setup
+ez_setup.use_setuptools()
 
 from setuptools import setup
 
 from App import __version__
 
+
 application_title = 'QBI AutoAnalysis SynapticVesicles'
-main_python_file = join('autoanalysis','App.py')
+main_python_file = join('.','App.py')
 venvpython = join(sys.prefix,'Lib','site-packages')
-mainpython = "D:\\Programs\\Python36"
+mainpython = "/Library/Frameworks/Python.framework/Versions/3.6/bin/python3"
 
 # Add info to MacOSX plist
 # plist = Plist.fromFile('Info.plist')
 plist = dict(CFBundleDisplayName=application_title,
              NSHumanReadableCopyright='Copyright (c) 2018 Queensland Brain Institute',
-             CFBundleTypeIconFile='autoanalysis/resources/measure.ico',
-             CFBundleVersion=__version__)
+             CFBundleTypeIconFile='autoanalysis/resources/newplot.ico',
+             CFBundleVersion=__version__
+            )
 
-APP = ['appgui.py']
+APP = ['App.py']
 DATA_FILES = ['autoanalysis/resources/']
+PARENTDIR= getcwd()
 OPTIONS = {'argv_emulation': True,
+           'use_pythonpath': True,
            'plist': plist,
-           'iconfile': 'autoanalysis/resources/measure.ico',
-           'includes': ['idna.idnadata', "numpy", "plotly", "packaging.version","packaging.specifiers", "packaging.requirements","appdirs",'scipy.spatial.cKDTree'],
-    'excludes': ['PyQt4', 'PyQt5'],
-    'packages': ['scipy','seaborn', 'numpy.core._methods', 'numpy.lib.format', 'plotly'],
-    'include_files': ['autoanalysis/', (join(venvpython, 'scipy', 'special', '_ufuncs.cp36-win32.pyd'), '_ufuncs.pyd')],
+           'iconfile': 'autoanalysis/resources/newplot.ico',
+           #'packages': ['sqlite3','scipy', 'wx'],
+           'bdist_base': join(PARENTDIR, 'build'),
+           'dist_dir': join(PARENTDIR, 'dist'),
            }
 
 setup(
